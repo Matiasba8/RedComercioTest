1f31e3de0828207e3201c35cd0e4e04a9a873623
webtech <webtech@webtech-debian.webtech>
2021-11-07 23:03:30 -0300

    SearchBoxOK

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,5 @@
 #   Character.create(name: 'Luke', movie: movies.first)
 
 
+u1 = User.create(email: "1@gmail.com", password: "123")
+adv1 = Advance.create(user_id: u1.id, debtor: "empresa1", date: "2017-21-01", invoice_number: 1,advance_per: 2, status: "procesando", final_amount: 123)
